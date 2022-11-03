e94d03e44778c479dea8e93dacbe1d75997ae111
marius93rm <marius93rm@gmail.com>
2022-11-03 09:39:10 +0100

    updated: bug placeholder

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'; // Import AsyncStorage
+import Input from './components/Input';
+
+const STORAGE_KEY = 'ASYNC_STORAGE_EXAMPLE_KEY'; // Define a key to store our data
 
 export default function App() {
+  const [name, setName] = useState('World');
+
+
+  const saveName = async (name) => { // Save name to AsyncStorage
+    try { // Try to save the name
+      await AsyncStorage.setItem(STORAGE_KEY, name); // Save the name
+      setName(name); // Update the name in the state
+    } catch (e) {
+      alert('Failed to save the data to the storage');
+    }
+  };
+
+  const loadName = async () => {
+    try {
+      const user = await AsyncStorage.getItem(STORAGE_KEY); // Get the name from AsyncStorage
+
+      if (user === null) return;
+
+      setName(user); // Update the name in the state
+
+    } catch (e) {
+      alert('Failed to fetch the data from storage');
+    }
+  };
+
+  useEffect(() => {
+    loadName();
+  }, []);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Input
+        placeholder="Enter your name"
+        onSubmitEditing={(value) =>
+          saveName(value)} />
+      <Text style={styles.text}>Hello {name}!</Text>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +55,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 20,
+    padding: 15,
+    backgroundColor: '#EEB'
+  }
 });
---
+++ components/Input.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput } from 'react-native';
+
+export default function Input({placeholder, onSubmitEditing}) {
+    const [text, setText] = useState('');
+
+    return (
+        <TextInput 
+            style={styles.input}
+            value={placeholder}
+            onChangeText={(text) => setText(text)}
+            onSubmitEditing={() => {
+                if (!text) return // Don't submit if empty
+                onSubmitEditing(text)
+                setText('')
+            }
+        }
+        />
+    );
+}
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        width: 200,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+});
